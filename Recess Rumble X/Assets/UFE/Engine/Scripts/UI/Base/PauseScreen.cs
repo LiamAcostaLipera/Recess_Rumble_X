0dc6580871066c6f2973df893a9d0f0c147f3304
etziok <francokees94@gmail.com>
2021-08-27 00:03:31 -0300

    *corregidos timings de animacion *posiciones de animaciones *agachar funciona bien cuando se sostiene

--- Recess Rumble X/Assets/UFE/Engine/Scripts/UI/Base/PauseScreen.cs
+++ Recess Rumble X/Assets/UFE/Engine/Scripts/UI/Base/PauseScreen.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PauseScreen : UFEScreen {
 	public virtual void GoToMainMenu(){
         UFE.PauseGame(false);
 		UFE.StartMainMenuScreen();
+		
 	}
 
 	public virtual void ResumeGame(){
 		UFE.PauseGame(false);
+	}
+
+	public void restartscene()
+	{
+		SceneManager.LoadScene(0);
 	}
 }
